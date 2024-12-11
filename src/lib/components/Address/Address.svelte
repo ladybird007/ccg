4ea7c28f13677b1ef0abc4696a27546c77c17bba
gorodnichaya-av <gorodnichaia.av@gmail.com>
2024-12-11 11:15:56 +0200

    Address component

---
+++ src/lib/components/Address/Address.svelte
@@ -0,0 +1,15 @@
+<script>
+  import '../Address/address.css';
+</script>
+
+<div class="address">
+  <p>
+    <a class="text-black address__link" href="https://maps.app.goo.gl/S25tUb1wbBWCx4GY9" target="_blank">
+      <span>3616 7th Ct S</span>  <br>
+      <span>Birmingham, AL 35222</span>
+    </a>
+  </p>
+  <p>
+    <a href="tel:2059391233">(205) 939-1233</a>
+  </p>
+</div>
--- src/lib/components/Footer/Footer.svelte
+++ src/lib/components/Footer/Footer.svelte
@@ -1,4 +1,6 @@
 <script>
+  import Address from '../Address/Address.svelte';
+
   import Linkedin from '$lib/assets/images/socials/Linkedin.svelte';
   import Youtube from '$lib/assets/images/socials/Youtube.svelte';
   import Facebook from '$lib/assets/images/socials/Facebook.svelte';
@@ -46,17 +48,7 @@
             </a>
           </div>
 
-          <div class="address">
-            <p>
-              <a class="text-black address__link" href="https://maps.app.goo.gl/S25tUb1wbBWCx4GY9" target="_blank">
-                <span>3616 7th Ct S</span>  <br>
-                <span>Birmingham, AL 35222</span>
-              </a>
-            </p>
-            <p>
-              <a href="tel:2059391233">(205) 939-1233</a>
-            </p>
-          </div>
+          <Address />
         </div>
       </div>
     </div>
