05851705633701375f411d3f518e3f3cd7cbf6b2
gorodnichaya-av <gorodnichaia.av@gmail.com>
2024-12-19 19:00:14 +0200

    Book a call

--- src/routes/book-a-call/+page.svelte
+++ src/routes/book-a-call/+page.svelte
@@ -22,15 +22,15 @@
           <p class="text-emphasis">
             From Medicare Advantage strategies to industry-disrupting apps, we can help strategize a plan of action based on your business needs. We fully scope and guarantee our quotes so there are never any surprises.
           </p>
-          <div class="speack-with">
-            <div>
+          <div class="speak-with">
+            <div class="speak-with__text">
               <p class="text-black">Speak with Brianna</p>
               <p class="text-family">
                 We're real people. Never bots. Reach out for an introductory call.
               </p>
               <a href="/contact-us" class="btn btn--outline">Contact Brianna</a>
             </div>
-            <div>
+            <div class="speak-with__photo">
               <img src={Brianna} alt="">
             </div>
           </div>
@@ -40,7 +40,7 @@
             <h3 class="from__headline">Fill out the form to book a call today.</h3>
             <form action="#">
               <div class="form__grid">
-                <div>
+                <div class="form__row">
                   <div>
                     <label for="first-name">First name*</label>
                     <input type="text" name="fisrt-name" placeholder="Sam" required />
@@ -50,7 +50,7 @@
                     <input type="text" name="last-name" placeholder="Humphries" required />
                   </div>
                 </div>
-                <div>
+                <div class="form__row">
                   <div>
                     <label for="email">Your Email Address *</label>
                     <input type="email" name="email" placeholder="info@ccgideas.com" required />
@@ -60,7 +60,7 @@
                     <input type="number" name="phone" placeholder="(205) 123-4567" />
                   </div>
                 </div>
-                <div>
+                <div class="form__row">
                   <div>
                     <label for="company-name">Company Name</label>
                     <input type="text" name="company-name" placeholder="Company LLC" />
@@ -76,41 +76,47 @@
                 </div>
                 <div>
                   <p class="text-semibold">How do you think we could help?</p>
-                  <ul>
+                  <ul class="checkbox-list">
                     <li>
                       <label class="checkbox">
                         <input type="checkbox">
-                        <span class="checkbox_icon"></span>
+                        <span class="checkbox__icon"></span>
+                        Business Strategy
                       </label>
                     </li>
                     <li>
                       <label class="checkbox">
                         <input type="checkbox">
-                        <span class="checkbox_icon"></span>
+                        <span class="checkbox__icon"></span>
+                        Website Design
                       </label>
                     </li>
                     <li>
                       <label class="checkbox">
                         <input type="checkbox">
-                        <span class="checkbox_icon"></span>
+                        <span class="checkbox__icon"></span>
+                        Branding
                       </label>
                     </li>
                     <li>
                       <label class="checkbox">
                         <input type="checkbox">
-                        <span class="checkbox_icon"></span>
+                        <span class="checkbox__icon"></span>
+                        Digital Marketing
                       </label>
                     </li>
                     <li>
                       <label class="checkbox">
                         <input type="checkbox">
-                        <span class="checkbox_icon"></span>
+                        <span class="checkbox__icon"></span>
+                        Mobile App Design
                       </label>
                     </li>
                     <li>
                       <label class="checkbox">
                         <input type="checkbox">
-                        <span class="checkbox_icon"></span>
+                        <span class="checkbox__icon"></span>
+                        Website Audit
                       </label>
                     </li>
                   </ul>
