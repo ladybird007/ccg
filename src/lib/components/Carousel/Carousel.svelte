a969502154ef0e4d4f38518040ccf33cde5ec7e7
gorodnichaya-av <gorodnichaia.av@gmail.com>
2024-12-20 16:10:13 +0200

    Carousel started

---
+++ src/lib/components/Carousel/Carousel.svelte
@@ -0,0 +1,45 @@
+<script lang=ts>
+	import emblaCarouselSvelte from 'embla-carousel-svelte';
+
+  import './carousel.css';
+  
+  // type EmblaCarouselType = NonNullable<UseEmblaCarouselType[1]>;
+
+  interface Props {
+    images?:{
+      imgUrl: string
+    }[],
+    testimonials?:{
+      text: string,
+      author: string,
+      position: string
+    }[]
+  }
+
+  let { images, testimonials }:Props = $props();
+  
+</script>
+	
+<div class="embla" use:emblaCarouselSvelte>
+  <div class="embla__container">
+    {#if images}
+      {#each images as image}
+        <div class="embla__slide">
+          <img src={image.imgUrl} alt="">
+        </div>
+      {/each}
+    {/if}
+    {#if testimonials}
+      {#each testimonials as item}
+        <div class="embla__slide">
+          <p class="font-family">“{item.text}”</p>
+          <p>
+            <span class="text-semibold">{item.author}</span>
+            <span class="text-light">{item.position}</span>
+          </p>
+        </div>
+      {/each}
+    {/if}
+  </div>
+
+</div>
