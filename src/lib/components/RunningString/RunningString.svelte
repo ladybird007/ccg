586f9b321d8790dbcf58800236910d3a137295a8
gorodnichaya-av <gorodnichaia.av@gmail.com>
2024-12-29 09:05:33 +0200

    Running text

--- src/lib/components/RunningString/RunningString.svelte
+++ src/lib/components/RunningString/RunningString.svelte
@@ -1,107 +1,63 @@
 <script lang=ts>
+  import { gsap } from "gsap";
+  import { ScrollTrigger } from "gsap/ScrollTrigger";
+  import { Observer } from "gsap/Observer";
   import { onMount } from 'svelte';
   import './running-string.css';
+  gsap.registerPlugin(ScrollTrigger);
+  gsap.registerPlugin(Observer);
 
   interface Props {
-    runningText?: string
+    runningText: string
   }
+  
   let  { runningText }: Props = $props();
 
-
-
-  let lastScrollTop = 0;
-
-  function move() {
-    const elem = document.querySelector('.running-string__inner');
-    let elemIndex = 0,
-          elemStep = 3,
-          elemWidth = elem.clientWidth + 1;
-
-    elemIndex = elemIndex < elemWidth ? elemIndex + elemStep : 1;
-
-    // console.log(elemWidth)
-
-
-    // elem.style.transform = `translateX(-${elemIndex}px)`;
-    
-  }
-
-  function isVisible(elem) {
-    let windowScroll = window.pageYOffset;
-    let position = elem.getBoundingClientRect();
-
-    if (position.top >= 0 && position.bottom <= window.innerHeight) {
-      elem.classList.add('visible');
-      
-      /*let st = window.pageYOffset || document.documentElement.scrollTop;
-      if (st > lastScrollTop) {
-        // console.log('down');
-      } else if (st < lastScrollTop) {
-        // console.log('up')
-      }
-      lastScrollTop = st <= 0 ? 0 : st;*/
-
-      elem.querySelector('.running-string__inner').style.transform = `translateX(-${windowScroll}px)`;
-    } else {
-      elem.classList.remove('visible');
-    }
-  };
-  
-
-  onMount(() => {
-
-    let didScroll = false;
-
-    window.addEventListener('scroll', function() {
-      didScroll = true;
-
-      const RunningString = document.querySelectorAll('.running-string');
-      RunningString.forEach((arrItem) => {
-        isVisible(arrItem);
+  onMount(() => {      
+    const rollConst = roll(".rollingText", {duration: 20});               
+    // helper function that clones the targets, places them next to the original, then animates the xPercent in a loop to make it appear to roll across the screen in a seamless loop.
+    function roll(targets, vars, reverse) {
+      const tl = gsap.timeline({
+        repeat: -1,
+        onReverseComplete() { 
+          this.totalTime(this.rawTime() + this.duration() * 20); // otherwise when the playhead gets back to the beginning, it'd stop. So push the playhead forward 10 iterations (it could be any number)
+        }
       });
-
-    }, false);
-    
-    const runStringInner = document.querySelector('.running-string__inner');
-    setInterval(() => {
-      if ( didScroll ) {
-        didScroll = false;
-      } else {
-        requestAnimationFrame(move);
-      }
-    }, 2500);
-
-
-    const runningInner = [...document.querySelectorAll('.running-string__inner')];
-    runningInner.forEach((inner) => {
-      inner.innerHTML = `${inner.innerHTML}`.repeat(30);
-    });
-
-
-    /* const marquees = [...document.querySelectorAll('.running-string__inner')];
-    marquees.forEach((marquee) => {
-        marquee.i = 0;
-        marquee.step = 3;
-        marquee.width = marquee.clientWidth + 1;
-        marquee.innerHTML = `${marquee.innerHTML}`.repeat(50);
-    });
-
-    requestAnimationFrame(move);
-
-    function move() {
-      marquees.forEach((marquee) => {
-          marquee.style.transform = `translate3d(-${marquee.i}px, 0, 0)`;
-          marquee.i = marquee.i < marquee.width ? marquee.i + marquee.step : 1;
+      vars = vars || {};
+      vars.ease || (vars.ease = "none");
+      gsap.utils.toArray(targets).forEach(el => {
+        let clone = el.cloneNode(true);
+        el.parentNode.appendChild(clone);
+        gsap.set(clone, {position: "absolute", top: el.offsetTop, left: el.offsetLeft + (reverse ? -el.offsetWidth : el.offsetWidth)});
+        tl.to([el, clone], {xPercent: reverse ? 100 : -100, ...vars}, 0);
       });
+      return tl;
     }
-    */
+
+    Observer.create({
+      onChangeY(self) {
+        let factor = 2.5;
+        if (self.deltaY < 0) {
+          factor *= -1;
+        } 
+        gsap.timeline({
+          defaults: {
+            ease: "none",
+          }
+        })
+          .to(rollConst, { timeScale: factor * 2.5, duration: 0.2 })
+          .to(rollConst, { timeScale: factor / 2.5, duration: 1 }, "+=0.3");
+      }
+    });    
   });
+
+ 
 </script>
 
 <div class="running-string">
   <div class="running-string__inner">
-    <span class="running-string__item">
-      {#if runningText}{runningText}{/if}
+    <span class="running-string__item rollingText">
+      {runningText}  
     </span>
   </div>
 </div>
