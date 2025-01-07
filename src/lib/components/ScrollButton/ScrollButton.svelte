57efc6819c3af8ed5133886298f209e42aaddc2d
gorodnichaya-av <gorodnichaia.av@gmail.com>
2025-01-07 13:14:24 +0200

    Top section: spacing

--- src/lib/components/ScrollButton/ScrollButton.svelte
+++ src/lib/components/ScrollButton/ScrollButton.svelte
@@ -3,7 +3,7 @@
 
   const scrollToNextSection = () => {
     const headerHeight = document.querySelector('.header').offsetHeight,
-          nextSectionTop = document.querySelector('.top-section').nextElementSibling.offsetTop + headerHeight;
+          nextSectionTop = document.querySelector('.top-section').nextElementSibling.offsetTop - headerHeight;
 
     window.scrollTo({ top: nextSectionTop, behavior: 'smooth' });
     return false
